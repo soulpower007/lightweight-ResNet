b346858a8f8372a2e3af09cb0a2659441f305419
HenleyHwang <hwangtianyu@hotmail.com>
2024-04-08 21:05:15 -0400

    Update inference.py for 3 residual layers

--- inference.py
+++ inference.py
@@ -60,8 +60,7 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
+        self.linear = nn.Linear(256 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -76,14 +75,13 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
-net = ResNet(BasicBlock, [2, 1, 1, 1])
+net = ResNet(BasicBlock, [4, 4, 3])
 ######################################################################################################
 
 
